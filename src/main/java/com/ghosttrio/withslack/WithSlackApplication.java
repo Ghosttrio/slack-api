1066bfc1d3626881f9e9d32e2816050b2ef193d6
Ghosttrio <ghosttrio@naver.com>
2024-09-13 13:32:28 +0900

    first commit

---
+++ src/main/java/com/ghosttrio/withslack/WithSlackApplication.java
@@ -0,0 +1,13 @@
+package com.ghosttrio.withslack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithSlackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithSlackApplication.class, args);
+	}
+
+}
